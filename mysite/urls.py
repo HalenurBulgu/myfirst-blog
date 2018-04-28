dc92003c0c3f20ea2d384e7ba5a7afa0fc4ebb69
halenur <h.bulgu@gmail.com>
2018-04-28 15:58:43 +0300

    Site için HTML dosyasını değiştirdim.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,5 @@
 """mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information please see:The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
@@ -13,9 +12,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
+
